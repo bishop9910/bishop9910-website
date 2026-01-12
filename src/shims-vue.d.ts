fba55bfba2075cf5fb641e7b5f9cf0e9d09afb90
bishop9910 <bishop9910@163.com>
2026-01-12 18:59:00 +0800

    删除注释

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,15 +1,7 @@
 // shims-vue.d.ts
 
-// 声明模块
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
-
-  // 定义组件的类型：
-  // 1. Props ({} - 这里是空对象，表示没有 props，但 AudioPlayer 有 props，所以这里应该更通用)
-  // 2. 原始绑定 ({} - 默认没有原始绑定)
-  // 3. 实例类型 (any)
   const component: DefineComponent<{}, {}, any>;
-  
-  // 导出组件
   export default component;
 }
